56552faae8de3a7e7ef6300c255ec256c0fb346b
JJ <infernoj2581@gmail.com>
2017-11-14 08:15:43 -0700

    Programmed the labels for the Impact page

--- Juan_CSP/Controller/ImpactController.swift
+++ Juan_CSP/Controller/ImpactController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+
 public class ImpactController: UIViewController{
     @IBOutlet weak var imageViewer: UIImageView!
     @IBOutlet weak var LabelViewer: UILabel!
+    @IBOutlet weak var popupImage: UIImageView!
+    @IBOutlet weak var labelTwo: UILabel!
+    @IBOutlet weak var labelThree: UILabel!
     
-    
+    private func setupAssets() -> Void{
+        popupImage.image = UIImage(named: "womanApp_plan")
+        self.labelTwo.text = "Our app is a planned to survey the particpant and ask them for their job title, and salary."
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     override public func viewDidLoad(){
         super.viewDidLoad()
+        setupAssets()
     }
 }
