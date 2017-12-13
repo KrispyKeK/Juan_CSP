ca4e21f926289dca838349ff54d25d87ceeb6f27
JJ <infernoj2581@gmail.com>
2017-12-13 08:39:33 -0700

    Added code to the programming file

--- Juan_CSP/Controller/ProgrammingController.swift
+++ Juan_CSP/Controller/ProgrammingController.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2017 Dela Cruz, Juan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+public class ProgrammingController: UIViewController{
+    override public func viewDidLoad() -> Void {
+        super.viewDidLoad()
+        let gameScene = StartScene(size: view.bounds.size)
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
+    }
+}
